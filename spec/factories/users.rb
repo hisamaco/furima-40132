518c7262bd37698975b0b966c112cf762a6c837f
hisamaco <hisabiz23@gmail.com>
2023-12-29 17:06:46 +0900

    passwordカラム修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              { Faker::Name.initials }
     email                 { Faker::Internet.email }
-    password              { Faker::Internet.password(min_length: 6, mix_case: true) }
+    password              { '1a' + Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
     last_name             { '胃そノ' }
     first_name            { 'カつ尾' }
