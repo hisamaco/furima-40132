a99bc8869003802495bc2d8830d50107b6d63ef4
hisamaco <hisabiz23@gmail.com>
2023-12-30 15:36:03 +0900

    商品情報登録機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,22 +6,22 @@ class Item < ApplicationRecord
   belongs_to :condition
   belongs_to :shipping_cost
   belongs_to :prefecture
-  belongs_to :day_to_ship
+  belongs_to :days_to_ship
 
-  with_options presence: true do
-    validates :image,             message: '画像は必須です'
-    validates :product_name,      message: '商品名は必須です'
-    validates :description,       message: '商品の説明は必須です'
-    validates :price,             message: '価格は必須です'
+  with_options presence: { message: 'は必須です' } do
+    validates :image
+    validates :product_name
+    validates :description
+    validates :price
     validates :user_id
   end
 
-  with_options numericality: { other_than: 1 } do
-    validates :category_id,       message: 'カテゴリーは必須です'
-    validates :condition_id,      message: '商品の状態は必須です'
-    validates :shipping_cost_id,  message: '配送料は必須です'
-    validates :prefecture_id,     message: '発送元の地域は必須です'
-    validates :day_to_ship_id,    message: '発送までの日数は必須です'
+  with_options numericality: { other_than: 1, message: 'は必須です' } do
+    validates :category_id
+    validates :condition_id
+    validates :shipping_cost_id
+    validates :days_to_ship_id
+    validates :prefecture_id
   end
 
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
