7d28d0236fca5928365a0e19c816733809e046db
hisamaco <hisabiz23@gmail.com>
2024-01-08 14:05:31 +0900

    購入情報の保存機能実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,23 @@
 class OrdersController < ApplicationController
   def index
-    @order_shipment = OrderShipments.new
     @item = Item.find(params[:item_id])
-  end
-
-  def new
+    @order_shipment = OrderShipment.new
   end
 
   def create
+    @order_shipment = OrderShipment.new(order_params)
+    if @order_shipment.valid?
+      @order_shipment.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_shipment).permit(:postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number)
+          .merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
--- app/models/order_shipment.rb
+++ app/models/order_shipment.rb
@@ -1,10 +1,11 @@
-class OrderShipments
+class OrderShipment
   include ActiveModel::Model
-  attr_accessor :potal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number, :user_id, :item_id
+
+  attr_accessor :postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number, :user_id, :item_id
 
   with_options presence: true do
     validates :postal_code
-    validates :prefecuture_id
+    validates :prefecture_id
     validates :city
     validates :street_address
     validates :phone_number
@@ -13,10 +14,7 @@ class OrderShipments
   end
 
   def save
-    # 寄付情報を保存し、変数donationに代入する
-    orders = Orders.create(user_id: user_id, item_id: item_id)
-    # 住所を保存する
-    # donation_idには、変数donationのidと指定する
-    Shipments.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, street_address: street_address, building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Shipment.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, street_address: street_address, building_name: building_name, phone_number: phone_number, order_id: order.id)
   end
 end
