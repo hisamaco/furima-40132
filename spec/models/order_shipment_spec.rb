7c859f11e07419df49d254f0a7b8ef16e164aece
hisamaco <hisabiz23@gmail.com>
2024-01-09 15:30:01 +0900

    user_idとitem_idを@order_shipmentに追加

--- spec/models/order_shipment_spec.rb
+++ spec/models/order_shipment_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe OrderShipment, type: :model do
   before do
-    @order_shipment = FactoryBot.build(:order_shipment)
+    user = FactoryBot.create(:user)
+    item = FactoryBot.create(:item)
+    @order_shipment = FactoryBot.build(:order_shipment, user_id: user.id, item_id: item.id)
   end
 
   describe '配送先情報の保存' do
